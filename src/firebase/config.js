240cd11a20ae027bdefb1431b43bdd5b49ed6e4d
David <jefferson-education@outlook.com>
2023-07-27 18:59:42 -0500

    Firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Web app Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB2ljV5Dk7ZxYVnVzp02YS0gcgq9CUFBGE",
+  authDomain: "image-uploader-80cc2.firebaseapp.com",
+  projectId: "image-uploader-80cc2",
+  storageBucket: "image-uploader-80cc2.appspot.com",
+  messagingSenderId: "352358119155",
+  appId: "1:352358119155:web:2083a1898f371cd9afa986",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
